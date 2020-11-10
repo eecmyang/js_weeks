2ab0b50c5b0017ed0bb69b74bd2292bdf39a9639
eecmyang <51515246+eecmyang@users.noreply.github.com>
2020-11-10 20:10:15 +0800

    Update book_index.js

--- week7/nkust/book_index.js
+++ week7/nkust/book_index.js
@@ -6,18 +6,18 @@ function getBoughtBook(books){
 	
 	if(degree=="大一"){
 		index = prompt("購買書籍號碼"+"\n1."+books[0]+"\n2."+books[1]+"\n9."+"結帳");
-		return index;
+		return index==9?9:parseInt(index);
 	}
 	else if(degree=="大二"){
 		index = prompt("購買書籍號碼"+"\n3."+books[2]+"\n4."+books[3]+"\n9."+"結帳");
-		return index;
+		return index==9?9:parseInt(index)+2;
 	}
 	else if(degree=="大三"){
 		index = prompt("購買書籍號碼"+"\n5."+books[4]+"\n6."+books[5]+"\n9."+"結帳");
-		return index;
+		return index==9?9:parseInt(index)+4;
 	}
 	else if(degree=="大四"){
 		index = prompt("購買書籍號碼"+"\n7."+books[6]+"\n8."+books[7]+"\n9."+"結帳");
-		return index;
+		return index==9?9:parseInt(index)+6;
 	}
 }
